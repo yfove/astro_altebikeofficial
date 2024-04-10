a941432bb005aa51bb435e1a64f76cec226f0f6e
Curtis <curtis.ch.chat@gmail.com>
2024-04-10 10:30:42 -0600

    resolve hero rsp dsn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), react()],
   vite: {
     ssr: {
       noExternal: ["accessible-astro-components"],
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
