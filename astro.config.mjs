73f95c64c7362e210a00abb67eebc890122676c0
curtis chat <curtis.ch.chat@gmail.com>
2024-04-17 16:17:26 -0600

    initiate database astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { remarkExcerpt, remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
-
 import react from "@astrojs/react";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    // image({
-    //   serviceEntryPoint: "@astrojs/image/sharp",
-    // }),
-    mdx(),
-    react(),
-  ],
+  integrations: [tailwind(),
+  // image({
+  //   serviceEntryPoint: "@astrojs/image/sharp",
+  // }),
+  mdx(), react(), db()],
   vite: {
     ssr: {
-      noExternal: ["accessible-astro-components"],
-    },
+      noExternal: ["accessible-astro-components"]
+    }
   },
-
   markdown: {
     remarkPlugins: [remarkReadingTime, remarkExcerpt],
-    extendDefaultPlugins: true,
-  },
+    extendDefaultPlugins: true
+  }
 });
